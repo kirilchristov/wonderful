8b5fa32f36b87ac2a3bf5b325d78c087cb899a99
kirilchristov <kiro.mm@gmail.com>
2020-02-29 18:40:34 -0800

    init commit and current navbar work

---
+++ src/Dealership.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dealership(){
+  return (
+    <div className="dealership">
+      <h1>I am Dealership</h1>
+    </div>
+  );
+}
+
+export default Dealership;
