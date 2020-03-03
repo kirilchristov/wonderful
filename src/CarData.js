503f96640c46ab58200de646dd74734acbff32a4
kirilchristov <kiro.mm@gmail.com>
2020-03-02 19:41:22 -0800

    added some lorem ipsum info for car descr

--- src/CarData.js
+++ src/CarData.js
@@ -2,7 +2,7 @@ const carData = [
   {
     id: 1,
     title: '2020 LS',
-    info: 'Built on the platform of the high-performance LC coupe, the LS 500 features our twin-turbocharged engine and offers meticulous craftsmanship like three premium wood-trim designs so unique they can take up to four months and 32 steps to complete.',
+    info: 'Pairing a potent engine with powerful electric motors and a self-charging lithium-ion battery, this is the highest performance LS Hybrid yet. Delivering instant torque, instinctive feel of a 10-speed automatic transmission, it is efficiency at the peak of luxury.',
     imageUrl: 'images/ls.jpg',
     price: '75,450',
     location: []
@@ -18,7 +18,7 @@ const carData = [
   {
     id: 3,
     title: '2020 LS F SPORT',
-    info: 'Built on the platform of the high-performance LC coupe, the LS 500 features our twin-turbocharged engine and offers meticulous craftsmanship like three premium wood-trim designs so unique they can take up to four months and 32 steps to complete.',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.',
     imageUrl: 'images/ls-f-sport.jpg',
     price: '81,450',
     location: []
