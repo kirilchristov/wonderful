4be93e8be4d63bc7ac19ed38f6f9cf6ac0dce567
kirilchristov <kiro.mm@gmail.com>
2020-03-02 19:42:00 -0800

    added handleClick and handleChange - changes the state

--- src/App.js
+++ src/App.js
@@ -8,13 +8,46 @@ import carData from './CarData'
 import './bootstrap.min.css'
 import './App.scss';
 
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedCar : carData[1]
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleClick = (id) => {  
+    this.setState({
+      selectedCar: carData[id-1]
+    })
+    console.log('Current selected car',this.state.selectedCar.title);
+  }
+
+  handleChange = (id) => {
+    this.setState({
+      selectedCar: carData[id]
+    })
+    console.log('LINE 34 APP.JS | I get as param...', id)
+  }
+
+  componentDidUpdate () {
+    console.log('Did Update selected car',this.state.selectedCar.title);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="container">
           <Header />
-          <Hero carData={carData}/>
+          <Hero 
+            carData={carData} 
+            handleClick={this.handleClick} 
+            selectedCar={this.state.selectedCar} 
+            handleChange={this.handleChange}/>
           <Dealership />
         </div>
       </div>
--- src/Hero.js
+++ src/Hero.js
@@ -10,34 +10,49 @@ function Hero(props){
   const car2 = carData[1]
   const car3 = carData[2]
 
-
-  console.log(car1)
   return (
+    
     <div className="hero">
       <Carousel 
       className="carousel-custom"
       selectedItem={1}
-      showIndicators={false}
+      // showIndicators={false}
       showThumbs={false}
+      showArrows={false}
       centerMode={true}
+      onChange={(e) => props.handleChange(e)}
       centerSlidePercentage={50}
+      statusFormatter={(curr, total)=>console.log(`Slide ${curr} of ${total}`)} 
       >
-        <div>
-          <img className="car-image" src={car1.imageUrl} alt={car1.title}/>
-          {/* <p className="legend">{car1.title}</p> */}
+        <div onClick={()=> props.handleClick(car1.id)}>
+          <img className="car-image" src={car1.imageUrl} alt={car1.title} />
         </div>
-        <div>
-          <img className="car-image" src={car2.imageUrl} alt={car1.title}/>
-          {/* <p className="legend">Legend 2</p> */}
+        <div onClick={()=> props.handleClick(car2.id)}>
+          <img className="car-image" src={car2.imageUrl} alt={car2.title} />
         </div>
-        <div>
-          <img className="car-image" src={car3.imageUrl}  alt={car1.title}/>
-          {/* <p className="legend">Legend 3</p> */}
+        <div onClick={()=> props.handleClick(car3.id)}>
+          <img className="car-image" src={car3.imageUrl}  alt={car3.title} />
         </div>
       </Carousel>
-      <p className="legend">{car1.title}</p>
+      <p className="legend">{props.selectedCar.title}</p>
+      <p className="price">STARTING AT ${props.selectedCar.price}</p>
+      <div className="info">
+        <p>{props.selectedCar.info}</p>
+      </div>
     </div>
   );
 }
 
 export default Hero;
+
+//Let's create dynamic element that pulls data
+  // const CarElement = carData.map((car,idx) => {
+  //   console.log('line 14 Hero',car)
+  //   return (
+  //     <div onClick={props.handleClick}>
+  //       <img className="car-image" src={car[idx].imageUrl}  alt={car[idx].title} />
+  //     </div>
+  //   )
+  // })
+
+
