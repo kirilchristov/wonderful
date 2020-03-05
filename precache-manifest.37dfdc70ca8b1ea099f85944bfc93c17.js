7c3177baa9ea119a17ae8c41f60b9f9d3738829b
kirilchristov <kiro.mm@gmail.com>
2020-03-05 13:18:41 -0800

    Updates

---
+++ precache-manifest.37dfdc70ca8b1ea099f85944bfc93c17.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b06774c6da15af30da5b90977b48c7a",
+    "url": "/wonderful/index.html"
+  },
+  {
+    "revision": "5fcd8042d17b7084a385",
+    "url": "/wonderful/static/css/2.e49e09ab.chunk.css"
+  },
+  {
+    "revision": "0d7781405a6d65d4d6fe",
+    "url": "/wonderful/static/css/main.277152e4.chunk.css"
+  },
+  {
+    "revision": "5fcd8042d17b7084a385",
+    "url": "/wonderful/static/js/2.d0db039e.chunk.js"
+  },
+  {
+    "revision": "a413935abd8e8922150d5cd77501c211",
+    "url": "/wonderful/static/js/2.d0db039e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7781405a6d65d4d6fe",
+    "url": "/wonderful/static/js/main.01e7e4ef.chunk.js"
+  },
+  {
+    "revision": "a132f4c0e34a39c2638e",
+    "url": "/wonderful/static/js/runtime-main.dea49c84.js"
+  },
+  {
+    "revision": "311b4c476224fe11c760b9045596ba18",
+    "url": "/wonderful/static/media/lexus-logo.311b4c47.png"
+  }
+]);
